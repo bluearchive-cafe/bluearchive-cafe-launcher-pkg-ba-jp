e733d26e4503dd6aa488eb3455a4d93b48eeee96
roundRekt <roundrekt@icloud.com>
2026-01-25 20:20:14 +0800

    添加resources.assets路由

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 export default {
-    async fetch(request, env, ctx) {
-        return Response.redirect(`https://launcher-pkg-ba-jp.yo-star.com${new URL(request.url).pathname}`, 302);
-    },
+  async fetch(request, env, ctx) {
+    const pathname = new URL(request.url).pathname;
+    const filename = pathname.split("/").pop();
+
+    if (filename === "resources.assets") {
+      const object = await env.DOWNLOAD.get(filename);
+      if (object) return new Response(object.body);
+    }
+
+    return Response.redirect(`https://launcher-pkg-ba-jp.yo-star.com${pathname}`,302);
+  },
 };
